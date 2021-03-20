4f0eedb2e0d36935670c652420cd16e609cea1d2
dharani <dharaniravikumar33@gmail.com>
2021-03-20 21:27:17 +0530

    added commentline

--- division/division.py
+++ division/division.py
@@ -1,6 +1,6 @@
 def dodivision(a,b):
-    division=a/b
-    print(division)
+    division=a/b#division of 2 numbers
+    print(division)#print the division
 a=4
 b=2
-dodivision(a,b)    
+dodivision(a,b) 
